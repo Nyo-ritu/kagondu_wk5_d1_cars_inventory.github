095bafe3281983197ed72281aa31295663e6b628
nyo-ritu <kagondu.wai@gmail.com>
2021-08-04 09:32:45 -0400

    first commit

--- cars_inventory/__init__.py
+++ cars_inventory/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
-# from config import Config
+from config import Config
 from .site.routes import site
+from .authentication.routes import auth
+from flask_migrate import Migrate
 
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
 app.register_blueprint(site)
+app.register_blueprint(auth)
--- config.py
+++ config.py
@@ -1,3 +1,7 @@
 import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+    SECRET_KEY = 'You will never guess....'
